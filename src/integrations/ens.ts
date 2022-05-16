0ceb39f82311f241f4964a49613476e9a53f06e9
ethboi <ethboi3@gmail.com>
2022-05-16 22:45:21 +0200

    Fix ens cache

--- src/integrations/ens.ts
+++ src/integrations/ens.ts
@@ -6,14 +6,16 @@ export async function GetEns(traderAddress: string): Promise<string> {
   console.log(traderAddress)
 
   if (found) {
-    console.log('found' + found)
+    console.log('found ' + found)
     return found
   }
 
   const ens = await mainNetInfuraProvider.lookupAddress(traderAddress)
 
   if (ens) {
-    global.LYRA_ENS.traderAddress = ens
+    global.LYRA_ENS[traderAddress] = ens
+  } else {
+    global.LYRA_ENS[traderAddress] = ''
   }
 
   return ens ? ens : ''
