45c5624d079390afb16f37704e0ec65a8b3da2e5
ethboi <ethboi3@gmail.com>
2022-06-21 23:40:55 +0200

    start hooking up lp stats

--- src/lyra/stats.ts
+++ src/lyra/stats.ts
@@ -1,20 +1,22 @@
 import Lyra, { Market } from '@lyrafinance/lyra-js'
 import { StatDto } from '../types/statDto'
+import fromBigNumber from '../utils/fromBigNumber'
 
-export async function GetStats(market: string, lyra: Lyra): Promise<StatDto> {
-  const marketObj = await Market.get(lyra, market)
+export async function GetStats(marketName: string, lyra: Lyra): Promise<StatDto> {
+  const market = await Market.get(lyra, marketName)
+  const liquidity = await market.liquidity
+  const greeks = await market.netGreeks()
 
   const stat: StatDto = {
-    asset: marketObj.name,
-    poolValue: 0,
+    asset: market.name,
+    poolValue: fromBigNumber(liquidity.nav),
     totalsUsd: 0,
     netCollateral: 0,
     netOptionsDelta: 0,
-    netDelta: 0,
-    netStdVega: 0,
+    netDelta: fromBigNumber(greeks.netDelta),
+    netStdVega: fromBigNumber(greeks.netStdVega),
     pnl: 0,
     timestamp: new Date(),
   }
-
   return stat
 }
