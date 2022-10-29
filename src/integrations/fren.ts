346f7e6cc6a01c29f64159a46710377567373e3c
ethboi <ethboi3@gmail.com>
2022-10-29 14:24:46 +0200

    remove for now

--- src/integrations/fren.ts
+++ src/integrations/fren.ts
@@ -3,20 +3,22 @@ import { urls } from '../constants/urls'
 import { FrenData } from '../types/fren'
 
 export const GetFren = async (search: string) => {
+  return undefined
+
   const found = global.FREN[search.toLowerCase()]
 
-  if (found || found === '') {
-    console.debug('fren found ' + found)
-    return found
-  }
+  // if (found || found === '') {
+  //   console.debug('fren found ' + found)
+  //   return found
+  // }
 
-  const frenData = (await axios.get(`${urls.etherleaderboardApiUrl}?q=${search}`)).data as FrenData
+  // const frenData = (await axios.get(`${urls.etherleaderboardApiUrl}?q=${search}`)).data as FrenData
 
-  if (frenData.count >= 1) {
-    global.FREN[search] = frenData.frens[0]
+  // if (frenData.count >= 1) {
+  //   global.FREN[search] = frenData.frens[0]
 
-    return frenData.frens[0]
-  }
+  //   return frenData.frens[0]
+  // }
 
-  return undefined
+  // return undefined
 }
