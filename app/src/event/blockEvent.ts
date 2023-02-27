483f6bd3929b7c7f0ba5087017c103a1755b1f44
ethboi <ethboi3@gmail.com>
2023-02-27 23:01:24 +0100

    undo test settings

--- app/src/event/blockEvent.ts
+++ app/src/event/blockEvent.ts
@@ -5,7 +5,7 @@ import { Context, Telegraf } from 'telegraf'
 import { Update } from 'telegraf/typings/core/types/typegram'
 import { TwitterApi } from 'twitter-api-v2'
 import { DEPOSIT_PROCESSED, STRIKE_ADDED, TRANSFER_TOPIC } from '../constants/topics'
-import { BTC_LIQUIDITY_POOL_ARB, CONTRACT_ADDRESSES } from '../constants/contractAddresses'
+import { CONTRACT_ADDRESSES } from '../constants/contractAddresses'
 import { Network } from '@lyrafinance/lyra-js'
 import { TrackTransfer } from '../lyra/tracker'
 import { TrackDeposits } from '../lyra/deposits'
@@ -34,19 +34,19 @@ export async function TrackEvents(
     lyra,
     async (event) => {
       if (event[0].topics[0].toLowerCase() === TRANSFER_TOPIC.toLowerCase()) {
-        // await TrackTransfer(discordClient, twitterClient1, event[0], network)
+        await TrackTransfer(discordClient, twitterClient1, event[0], network)
       }
       if (event[0].topics[0].toLowerCase() === DEPOSIT_PROCESSED.toLowerCase()) {
         await TrackDeposits(discordClient, discordClientBtc, twitterClient1, event[0], network)
       }
-      // if (event[0].topics[0].toLowerCase() === STRIKE_ADDED) {
-      //   await TrackStrikeAdded(discordClient, discordClientBtc, telegramClient, twitterClient, lyra, network, event)
-      // }
+      if (event[0].topics[0].toLowerCase() === STRIKE_ADDED) {
+        //await TrackStrikeAdded(discordClient, discordClientBtc, telegramClient, twitterClient, lyra, network, event)
+      }
     },
     {
       startBlockNumber: blockNumber,
-      addresses: [BTC_LIQUIDITY_POOL_ARB],
-      topics: [DEPOSIT_PROCESSED], //STRIKE_ADDED, ,DEPOSIT_PROCESSED //TRANSFER_TOPIC
+      addresses: CONTRACT_ADDRESSES,
+      topics: [DEPOSIT_PROCESSED, TRANSFER_TOPIC], //STRIKE_ADDED, ,DEPOSIT_PROCESSED //TRANSFER_TOPIC
       pollInterval: pollInterval,
     },
   )
