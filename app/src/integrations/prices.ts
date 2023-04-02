77c6a3b0aa0132a41e17a857d3c1d455615ccd20
ethboi <ethboi3@gmail.com>
2023-04-02 20:00:41 +0200

    fix lyra price retrieval

--- app/src/integrations/prices.ts
+++ app/src/integrations/prices.ts
@@ -1,29 +1,32 @@
 import axios from 'axios'
-import { ETH_OP, BTC_OP, LYRA_TOKENS } from '../constants/contractAddresses'
+import { ETH_OP, BTC_OP, LYRA_OP } from '../constants/contractAddresses'
 import { urls } from '../constants/urls'
 import { Dexscreener } from '../types/dexscreener'
 
 export async function GetPrices(): Promise<void> {
   try {
-    const addresses = [ETH_OP, BTC_OP, LYRA_TOKENS.LYRA_OP]
-    const dexscreenerData = (await axios.get(`${urls.dexscreenerUrl}${addresses.join(',')}`)).data as Dexscreener
-    console.log(dexscreenerData)
-    addresses.map((address) => {
+    const addresses = [LYRA_OP, ETH_OP, BTC_OP]
+    addresses.map(async (address) => {
+      const dexscreenerData = (await axios.get(`${urls.dexscreenerUrl}${address}`)).data as Dexscreener
       const pair = dexscreenerData.pairs.find((pair) => pair.baseToken.address.toLowerCase() == address.toLowerCase())
       if (pair) {
-        if (address.toLowerCase() == ETH_OP) {
+        if (address.toLowerCase() == ETH_OP.toLowerCase()) {
           global.ETH_PRICE = Number(pair.priceUsd)
           global.ETH_24HR = Number(pair.priceChange.h24)
+          //console.log(`New ETH PRICE: ${global.ETH_PRICE}`)
         }
-        if (address.toLowerCase() == BTC_OP) {
+        if (address.toLowerCase() == BTC_OP.toLowerCase()) {
           global.BTC_PRICE = Number(pair.priceUsd)
           global.BTC_24HR = Number(pair.priceChange.h24)
+          //console.log(`New BTC PRICE: ${global.BTC_PRICE}`)
         }
-        if (address.toLowerCase() == LYRA_TOKENS.LYRA_OP) {
+        if (address.toLowerCase() == LYRA_OP.toLowerCase()) {
           global.LYRA_PRICE = Number(pair.priceUsd)
-          //console.log(`LYRA: ${global.LYRA_PRICE}`)
           global.LYRA_24HR = Number(pair.priceChange.h24)
+          //console.log(`New LYRA PRICE: ${global.LYRA_PRICE}`)
         }
+      } else {
+        console.log(`Pair not found: ${address.toLowerCase()}`)
       }
     })
   } catch (error) {
